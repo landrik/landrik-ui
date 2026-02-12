c5ae9ad4c16645971b51143f04799bc154e8a3fc
Landry Karege <landry.karege@globaldata.com>
2026-02-12 16:50:35 +0000

    added CNAME

--- src/components/atoms/Pagination/index.ts
+++ src/components/atoms/Pagination/index.ts
@@ -1,5 +1,5 @@
 export { Pagination } from './Pagination';
-export type { PaginationProps, PaginationSize, PaginationVariant } from './Pagination.types';
+export type { PaginationProps, PaginationSize, PaginationVariant, PageButtonProps } from './Pagination.types';
 
 //export default Pagination;
 
